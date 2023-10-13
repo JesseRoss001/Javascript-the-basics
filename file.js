722f578d017d1e3ab9feee76f600f26c2f7db901
Jesse Lee Ross <jesseross001@gmail.com>
2023-10-13 23:30:23 +0000

    Intro stuff

---
+++ file.js
@@ -0,0 +1,28 @@
+var myName ="Jesse";
+
+let surname="Ross";
+
+const pi=3.14;
+
+var a;
+var b=2;
+console.log(b)
+a = 7;
+console.log(a)
+b = a;
+console.log(a)
+console.log(b)
+
+var a=9;
+
+var d = 10;
+var e = 11;
+var f = "I am a "
+
+d +=1
+console.log(d)
+e -=1 
+console.log(e)
+
+f= f + "string";
+console.log(f)
