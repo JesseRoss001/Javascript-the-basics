7574ba8c3a1a788de249eb052f4b9cdcd42ed69f
Jesse Lee Ross <jesseross001@gmail.com>
2023-10-18 21:32:13 +0000

    exercise with classnames

--- file.js
+++ file.js
@@ -104,7 +104,7 @@ document.body.onload = function() {
     console.log('We can call various methods of the body object as well, such as this one, body.onload, which gets called when loading is complete! Now we know that the body has been loaded!');
 }
 
-
+console.log ("%c Working with paragraphs \n ", "font-size:18px; color:lightgreen;")
 //paragraph notation as well grabbing by tag name 
 let firstParagraph = document.getElementsByTagName('p')[0];
 let numParagraphs = document.getElementsByTagName('p').length;
@@ -117,3 +117,44 @@ console.log(numParagraphs);
 console.log(secondParagraph);
 console.log(specificParagraph);
 console.log(allParagraphs); 
+
+
+console.log ("%c Paragraph Exercise \n ", "font-size:16px; color:orange;")
+
+let pElements = document.getElementsByTagName('p')
+console.log(pElements)
+let secondH1 = document.getElementsByTagName('h1')[2]; 
+console.log(secondH1)
+
+
+console.log ("%c Working with classnames  \n ", "font-size:18px; color:lightgreen;")
+
+let blueThings = document.getElementsByClassName('blue');
+
+// All items with class="blue"
+console.log('The HTMLCollection contains:', blueThings);
+
+// Iterate and print all their texts:
+for(let i = 0; i < blueThings.length; i++) {
+  console.log('Index ' + i + ' in the collection: ' + blueThings[i].textContent);
+}
+
+// Get a specific index:
+console.log("Index 2 is:", blueThings[2].textContent);
+
+// Same as above, but using the .item() method:
+console.log("Index 4 is:", blueThings.item(3).textContent);
+
+// Get the item with class="blue" and id="blue-div" and log its tag name:
+console.log('The item with id="blue-div" is a', blueThings.namedItem('blue-div').tagName, 'element.');
+
+// The other div is not in the collection since it doesn't have the "blue" class. The namedItem() method returns null
+console.log('The item with id="another-blue-div" is', blueThings.namedItem('another-blue-div'));
+
+
+console.log ("%c classname  Exercise \n ", "font-size:16px; color:orange;")
+
+let theGreenTeam=document.getElementsByClassName("green");
+for (let a=0 ; a < theGreenTeam.length; a++) {console.log("I am teamMember :"+ a + "," + theGreenTeam[a] + "AKA" + theGreenTeam[a].textContent)}
+let greenbean =document.getElementById("greenbean");
+console.log("They call me the team leader of the Green Team ." +theGreenTeam.namedItem('greenbean') + "my name is " + greenbean.textContent);
