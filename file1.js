fb27687ebf83024178e2f3ded2529020acbc8869
Jesse Lee Ross <jesseross001@gmail.com>
2023-10-19 09:18:42 +0000

    focused on getting specific parts like styles , outer and inner html from referencing ids.

--- file1.js
+++ file1.js
@@ -61,3 +61,53 @@ for (let a = 0; a < theChildren.length; a++) {
     console.log(a +  theChildren[a].textContent);
 }
 
+
+console.log("%c Getting Specific Element Properties ", "color: red; font-weight: bold; font-size:30px ; ");
+
+let box = document.getElementById('main-box');
+
+// All the box's content
+console.log(box.innerHTML);
+
+// You can use window.getComputedStyle and pass it an element to get that element's styles as a list of key/value pairs
+console.log("The box's shadow:", window.getComputedStyle(box).boxShadow);
+console.log("The box's border", window.getComputedStyle(box).border);
+console.log("The box's border radius", window.getComputedStyle(box).borderRadius);
+console.log(window.getComputedStyle(box).padding)
+// The box's height and width
+console.log('Box height is:', box.clientHeight);
+console.log('Box width is:', box.clientWidth);
+
+// The main heading
+let heading = box.children[0];
+
+// The heading's classes
+console.log(heading.classList);
+
+
+
+console.log(box.classList);
+
+// The input box
+let username = document.getElementById('username-input');
+
+// All the input's attributes
+console.log(username.attributes);
+
+// To get their name/value, we must iterate
+let attrs = username.attributes;
+for (let i = 0; i < attrs.length; i++) {
+    console.log('Attr name:', attrs[i].name, '- Attr value:', attrs[i].value);
+}
+
+
+console.log("%c Getting Specific Element Properties  Exercise ", "color: red; font-weight: bold; font-size:30px ; ");
+
+let  usernameInput = document.getElementById("username-input");
+console.log("This gives the outerHTML "+ usernameInput.outerHTML)
+let inputPadding = document.getElementById("username-input");
+console.log(getComputedStyle(inputPadding).padding);
+console.log(getComputedStyle(inputPadding).color);
+
+
+
